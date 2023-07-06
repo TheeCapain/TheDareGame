71505d352d610c616e3ae7527f0050c264b30650
August <Augusthauerslev@outlook.com>
2023-07-06 17:07:30 +0200

    added remove card logic

--- my-project/src/Components/cards/CardList.js
+++ my-project/src/Components/cards/CardList.js
@@ -2,34 +2,38 @@ import Card from "./Card";
 
 
 let cards = [{
-    title: "TestCard1",
-    description: "Testdescription1",
+    title: "Take a picture of an exotic house animal",
+    description: "Take a picture of an animal that is not a pigeon a seagull or a normal house pet.",
     points: 100
 }, {
-    title: "TestCard2",
-    description: "Testdescription2",
+    title: "Photoshoot with the little mermaid",
+    description: "Take a picture of yourself standing in front of the little mermaid",
     points: 200
 }, {
-    title: "TestCard3",
-    description: "Testdescription3",
+    title: "Treading waters",
+    description: "Stand 15m above the water level.",
     points: 300
 }]
+
+let usedCards = []
 
 let activeCard = () => {
     const randomIndex = Math.floor(Math.random() * cards.length);
     const card = cards[randomIndex];
+    cards.slice(randomIndex)
+    console.log(cards)
+    usedCards.push(card)
+    console.log(usedCards)
     return card;
-  };
+};
 
-
+const card = activeCard()
 
 function CardList() {
-    const title = activeCard().title 
-    const description = activeCard().description
-    const points = activeCard().points
+
     return (
         <div id='activeCard' className='grid h-screen place-items-center'>
-            <Card title={title} description={description} points={points}/>
+            <Card title={card.title} description={card.description} points={card.points} />
         </div>
     );
 }
