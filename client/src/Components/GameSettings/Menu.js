4ee484a384427a87ed655d9eede1080296d2fc1f
August <Augusthauerslev@outlook.com>
2023-08-04 16:07:26 +0200

    Some changes

--- client/src/Components/GameSettings/Menu.js
+++ client/src/Components/GameSettings/Menu.js
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min"
 
-
+let nextId = 0;
 
 function Menu() {
-    const [playerName, setPlayerName] = useState('')
 
-    let playerId = 1;
-    const [players, setPlayers] = useState([
+    const [name, setName] = useState('')
+    const [players, setPlayers] = useState([{
+        id: nextId,
+        name: "Test"
+    }
     ])
+    console.log(players)
 
     return (
         <div className="grid place-items-center">
             <div>
                 <ul >
                     <li><h1 className="font-sans">The Challenge Game</h1></li>
-                    <li><label className="left-0 ">Players</label></li>
-                    {players.map(player => (
-                        <input type="text" key={player.playerId} value={player.playerId} onChange={e => setPlayerName(e.target.value)} className="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Player Name" />
+                    <li><label className="left-0 ">Players</label></li>{players.map(player => (
+                        <input type="text" key={player.id} onChange={e => setName(e.target.value)} className="mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Player Name" value={player.name} />
                     ))}
                     <li>
-                        <button onClick={() => {
+                        <button className="bg-green-500 hover:bg-green-700 w-64 m-1 text-white font-bold py-2 px-4 rounded-full" onClick={() => {
                             setPlayers([
                                 ...players,
-                                { playerId: playerId++, playerName: playerName }
+                                { id: nextId++, name: name }
                             ]);
-                        }} className="bg-green-500 hover:bg-green-700 w-64 m-1 text-white font-bold py-2 px-4 rounded-full">
+                        }} >
                             +
                         </button>
                     </li>
